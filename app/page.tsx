b0b40ba72ac25010c95aa18347b6eb5ae8431dcd
CodeVoyager007 <sufyanmughal001100@gmail.com>
2024-10-01 18:47:07 +0500

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import WeatherWidget from "@/components/ui/weather-widget";
+import { Colors } from "chart.js";
 
 export default function Home() {
   return (
     <div>
       <WeatherWidget />
+      <div className="absolute bottom-4 right-4 text-white"  style={{ fontFamily: 'FancyScript Bold Italic' }}>
+        Made with <span className="text-red-950"> ♥</span> by Ayesha Mughal
+      </div>
     </div>
   );
 }
--- components/ui/weather-widget.tsx
+++ components/ui/weather-widget.tsx
@@ -183,6 +183,7 @@ export default function WeatherWidget() {
                   <Line data={chartData} />
                 </div>
               )}
+              
             </>
           )}
         </CardContent>
